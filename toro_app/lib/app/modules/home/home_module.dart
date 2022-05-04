1c2c80822d8061ba058f4582141d253c478c2ffc
Thiago Mohallem <thiago.mohallem@localiza.com>
2022-05-04 20:53:23 -0300

    Updating app files to modular initial structure

---
+++ toro_app/lib/app/modules/home/home_module.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'counter_cubit.dart';
+import 'home_page.dart';
+
+class HomeModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => CounterCubit()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute(Modular.initialRoute,
+        child: (context, args) => const HomePage()),
+  ];
+}
