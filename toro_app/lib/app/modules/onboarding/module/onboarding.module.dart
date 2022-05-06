a7ad5dbe7d04917bd6ef580f75d910a94f88d02f
Thiago Mohallem <thiago.mohallem@localiza.com>
2022-05-06 10:14:52 -0300

    Adding cubit binds to Onboarding Module

--- toro_app/lib/app/modules/onboarding/module/onboarding.module.dart
+++ toro_app/lib/app/modules/onboarding/module/onboarding.module.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:toro_app/app/modules/onboarding/ui/cubits/page_index.cubit.dart';
+import 'package:toro_app/app/modules/onboarding/ui/cubits/splash_logo_opacity.cubit.dart';
 import 'package:toro_app/app/modules/onboarding/ui/splash.page.dart';
 
 class OnboardingModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind.lazySingleton<PageIndexCubit>((i) => PageIndexCubit()),
+    Bind.lazySingleton<SplashTextOpacityCubit>((i) => SplashTextOpacityCubit()),
+  ];
 
   @override
   final List<ModularRoute> routes = [
