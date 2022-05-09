d1ae7e5e9295f2240176f174d8c8f6063b5c7fce
Thiago Mohallem <thiago.mohallem@localiza.com>
2022-05-08 23:18:55 -0300

    adapting get quotes usecase to include price history

--- toro_app/lib/app/modules/home/domain/usecases/get_quotes.usecase.dart
+++ toro_app/lib/app/modules/home/domain/usecases/get_quotes.usecase.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:toro_app/app/modules/home/domain/infra/quotes.repository.dart';
 import 'package:toro_app/app/modules/home/domain/models/stock_quote.model.dart';
-import 'package:toro_app/app/modules/home/utils/quotes_quicksort.dart';
 
 abstract class IGetQuotesUsecase {
   Future<Stream<List<StockQuote>>> call();
@@ -24,13 +23,16 @@ class GetQuotesUsecase implements IGetQuotesUsecase {
     final stockStream = await _quotesRepository.retrieveQuotes();
     stockStream.listen((stock) {
       StockQuote stockQuote = StockQuote(
-        stockId: stock.id,
-        currentPrince: stock.value,
-        openPrice: stock.value,
-        valuation: 0.0,
-      );
+          stockId: stock.id,
+          currentPrince: stock.value,
+          openPrice: stock.value,
+          valuation: 0.0,
+          timestamp: stock.timestamp);
       if (stockQuotes.contains(stockQuote)) {
         int indexOfQuote = stockQuotes.indexOf(stockQuote);
+        stockQuotes[indexOfQuote]
+            .priceHistory
+            .add(PriceHistory(timestamp: stock.timestamp, price: stock.value));
         stockQuotes[indexOfQuote].currentPrince = stock.value;
         stockQuotes[indexOfQuote].valuation =
             stockQuotes[indexOfQuote].openPrice != 0
@@ -40,7 +42,10 @@ class GetQuotesUsecase implements IGetQuotesUsecase {
       } else {
         stockQuotes.add(stockQuote);
       }
-      quickSort(stockQuotes, 0, stockQuotes.length - 1);
+      stockQuotes.sort((a, b) {
+        if (a.valuation == b.valuation) return 0;
+        return b.valuation.compareTo(a.valuation);
+      });
       _streamController.sink.add(stockQuotes);
     });
     return _streamController.stream;
